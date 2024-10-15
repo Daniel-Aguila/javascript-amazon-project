2b63a3656a4927e5113c787f1207d7eee4c3f1c9
Daniel Aguila <danielbaguila@gmail.com>
2024-10-14 19:30:19 -0500

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,54 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import {loadCart} from '../data/cart.js'
 
-loadProducts(() =>{
+//resolve lets us control when to go to the next step
+//we waited until loadProducts finishes, we can wait till one step finishes before going to next step
+
+//promises are a better way to wait for async functions
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(()=>{
+            resolve();
+        });
+    })
+
+]).then((value) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    });
+}).then((value) =>{
+    console.log(value);
+
+    return new Promise((resolve) => {
+        loadCart(()=>{
+            resolve();
+        });
+    });
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+//Promise vs callbakcs
+
+//multiple callbacks cause a lot of nesting
+//better to use promises
+
+//Promise.all() lets us run multiple promises at the same time
+//and wait for all of them to finish
